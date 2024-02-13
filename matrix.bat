71a8ed55cd39bc561005bbd6a5795d70642f63bf
Knockitup <122903946+Knockitup@users.noreply.github.com>
2024-02-12 21:03:02 -0500

    Add files via upload

---
+++ matrix.bat
@@ -0,0 +1,11 @@
+@echo off
+
+color 02
+
+mode 1000
+
+:matrixbynima
+
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+
+goto matrixbynima
